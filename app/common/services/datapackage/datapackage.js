4e08fdf263d1f5bfb8ea880deb471817c8850b25
Jayson Harshbarger <hypercubed@gmail.com>
2016-09-01 13:33:31 +0900

    Just a note

--- app/common/services/datapackage/datapackage.js
+++ app/common/services/datapackage/datapackage.js
@@ -1,3 +1,5 @@
+/* Note, must import chi-datapackage/dist because SystemJS does not support mixed ES6/CJS modules */
+
 const DataPackageService = require('chi-datapackage/dist/service');
 const Normalizer = require('chi-datapackage/dist/normalizer');
 
