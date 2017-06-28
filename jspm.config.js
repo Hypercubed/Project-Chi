c77f6c51ae6c2298290716fcc64c63e793cd073b
Maxime HEBRARD <maxime.hebrard@gmail.com>
2017-06-28 10:48:25 +0900

    Add npm:d3-interpolate

--- jspm.config.js
+++ jspm.config.js
@@ -118,11 +118,12 @@ SystemJS.config({
     "d3": "npm:d3@3.5.17",
     "d3-array": "npm:d3-array@1.0.1",
     "d3-axis": "npm:d3-axis@1.0.3",
-    "d3-color": "npm:d3-color@1.0.1",
+    "d3-color": "npm:d3-color@1.0.3",
     "d3-dispatch": "npm:d3-dispatch@1.0.1",
     "d3-drag": "npm:d3-drag@1.0.1",
     "d3-force": "npm:d3-force@1.0.3",
     "d3-hierarchy": "npm:d3-hierarchy@1.0.2",
+    "d3-interpolate": "npm:d3-interpolate@1.1.5",
     "d3-plugins": "github:d3/d3-plugins@master",
     "d3-scale": "npm:d3-scale@1.0.3",
     "d3-scale-chromatic": "npm:d3-scale-chromatic@1.0.2",
@@ -818,7 +819,7 @@ SystemJS.config({
     },
     "npm:d3-interpolate@1.1.1": {
       "map": {
-        "d3-color": "npm:d3-color@1.0.1"
+        "d3-color": "npm:d3-color@1.0.3"
       }
     },
     "npm:ml-pca@1.1.2": {
@@ -1097,7 +1098,7 @@ SystemJS.config({
     },
     "npm:d3-transition@1.0.3": {
       "map": {
-        "d3-color": "npm:d3-color@1.0.1",
+        "d3-color": "npm:d3-color@1.0.3",
         "d3-interpolate": "npm:d3-interpolate@1.1.1",
         "d3-timer": "npm:d3-timer@1.0.3",
         "d3-ease": "npm:d3-ease@1.0.1",
@@ -1124,7 +1125,7 @@ SystemJS.config({
     "npm:d3-scale@1.0.3": {
       "map": {
         "d3-array": "npm:d3-array@1.0.1",
-        "d3-color": "npm:d3-color@1.0.1",
+        "d3-color": "npm:d3-color@1.0.3",
         "d3-collection": "npm:d3-collection@1.0.1",
         "d3-interpolate": "npm:d3-interpolate@1.1.1",
         "d3-time": "npm:d3-time@1.0.4",
@@ -1240,6 +1241,11 @@ SystemJS.config({
     "npm:sha.js@2.4.8": {
       "map": {
         "inherits": "npm:inherits@2.0.3"
+      }
+    },
+    "npm:d3-interpolate@1.1.5": {
+      "map": {
+        "d3-color": "npm:d3-color@1.0.3"
       }
     }
   }
